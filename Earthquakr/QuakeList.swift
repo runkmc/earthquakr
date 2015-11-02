88e7d98493f86a6f334615dd309887bbedc9e9a0
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-02 10:39:44 -0800

    Create QuakeList and matching Spec

---
+++ Earthquakr/QuakeList.swift
@@ -0,0 +1,9 @@
+//
+//  QuakeList.swift
+//  Earthquakr
+//
+//  Created by Kevin McGladdery on 11/2/15.
+//  Copyright © 2015 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
---
+++ EarthquakrTests/QuakeListSpec.swift
@@ -0,0 +1,18 @@
+//
+//  QuakeListSpec.swift
+//  Earthquakr
+//
+//  Created by Kevin McGladdery on 11/2/15.
+//  Copyright © 2015 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+@testable import Earthquakr
+
+class QuakeListSpec : QuickSpec {
+  override func spec() {
+    
+  }
+}

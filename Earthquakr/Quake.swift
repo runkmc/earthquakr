34f671675678146561d64e824694f6415901cb54
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-01 21:42:07 -0800

    Quake has a Location String

--- Earthquakr/Quake.swift
+++ Earthquakr/Quake.swift
@@ -10,4 +10,5 @@ import Foundation
 
 struct Quake {
   let magnitude: Double
+  let locationString: String
 }
--- EarthquakrTests/QuakeSpec.swift
+++ EarthquakrTests/QuakeSpec.swift
@@ -15,9 +15,15 @@ class QuakeSpec: QuickSpec {
   override func spec() {
     describe("the essence of a quake") {
       let magnitude = 2.5
-      let quake = Quake(magnitude: magnitude)
+      let locationString = "4km ESE of San Ramon, California"
+      let quake = Quake(magnitude: magnitude, locationString: locationString)
+      
       it("has a magnitude") {
         expect(quake.magnitude) == 2.5
+      }
+      
+      it("has a location string") {
+        expect(quake.locationString) == locationString
       }
     }
   }

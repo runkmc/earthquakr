5b07d616d44a5b9c36186093652631204d2b9c50
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-04 10:37:38 -0800

    QuakeList takes an array of Quakes and produces an array of QuakeViewModels

--- Earthquakr/QuakeList.swift
+++ Earthquakr/QuakeList.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+
+struct QuakeList {
+  let quakeViewModels: [QuakeViewModel]
+  
+  init(quakes: [Quake]) {
+    self.quakeViewModels = quakes.map { QuakeViewModel(quake: $0) }
+  }
+}
--- EarthquakrTests/QuakeListSpec.swift
+++ EarthquakrTests/QuakeListSpec.swift
@@ -14,5 +14,21 @@ import Nimble
 class QuakeListSpec : QuickSpec {
   override func spec() {
     
+    let date1 = Int(xHoursAgo(1)(date: NSDate()).timeIntervalSince1970 * 1000)
+    let quake1 = Quake(magnitude: 4.55, locationString: "0km ESE of Concord, California", rawTime:date1, url:"http://earthquake.usgs.gov/earthquakes/eventpage/ak10992884", coordinates:[-116.7776667,33.6633333,12.39])
+    let date2 = Int(xHoursAgo(2)(date: NSDate()).timeIntervalSince1970 * 1000)
+    let quake2 = Quake(magnitude: 4.55, locationString: "0km ESE of Concord, California", rawTime:date2, url:"http://earthquake.usgs.gov/earthquakes/eventpage/ak10992884", coordinates:[-121.958000,37.780000,12.39])
+    let date3 = Int(xHoursAgo(3)(date: NSDate()).timeIntervalSince1970 * 1000)
+    let quake3 = Quake(magnitude: 4.55, locationString: "0km ESE of Concord, California", rawTime:date3, url:"http://earthquake.usgs.gov/earthquakes/eventpage/ak10992884", coordinates:[-121,959000,37.780000,12.39])
+    let date4 = Int(xHoursAgo(4)(date: NSDate()).timeIntervalSince1970 * 1000)
+    let quake4 = Quake(magnitude: 4.55, locationString: "0km ESE of Concord, California", rawTime:date4, url:"http://earthquake.usgs.gov/earthquakes/eventpage/ak10992884", coordinates:[-121,957000,37.780000,12.39])
+    let quakes = [quake1, quake2, quake3, quake4]
+    
+    it("takes an array of Quakes and provides an array of QuakeViewModels") {
+      let list = QuakeList(quakes: quakes)
+      expect(list.quakeViewModels[0].magnitudeString) == "4.5"
+    }
+    
+    
   }
 }

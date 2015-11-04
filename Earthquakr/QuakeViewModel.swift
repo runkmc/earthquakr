af19cc2e38ffaeeed01086ce81c5b8820340023a
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-03 21:47:42 -0800

    QuakeViewModel formats the time

--- Earthquakr/QuakeViewModel.swift
+++ Earthquakr/QuakeViewModel.swift
@@ -12,7 +12,7 @@ import CoreLocation
 struct QuakeViewModel {
   let quake: Quake
   let magnitudeString: String
-//  let time: String
+  let time: String
 //  let url: NSURL
 //  let location: CLLocation
   var locationString: String {
@@ -22,5 +22,16 @@ struct QuakeViewModel {
   init(quake:Quake) {
     self.quake = quake
     self.magnitudeString = String(format: "%.1f", quake.magnitude)
+    self.time = QuakeViewModel.formatTime(quake.rawTime)
+  }
+  
+  static private func formatTime(time:Int) -> String {
+    let formatter = NSDateFormatter()
+    formatter.dateFormat = "h:mm a"
+    
+    let newTime = time / 1000
+    let date = NSDate(timeIntervalSince1970: Double(newTime))
+    
+    return formatter.stringFromDate(date)
   }
 }
--- EarthquakrTests/QuakeViewModelSpec.swift
+++ EarthquakrTests/QuakeViewModelSpec.swift
@@ -14,7 +14,7 @@ import Nimble
 class QuakeViewModelSpec: QuickSpec {
   override func spec() {
     describe("QuakeViewModel") {
-      let quake = Quake(magnitude: 1.29, locationString: "10km SSW of Pine Cove, California", rawTime: 1388620296020, url:"http://earthquake.usgs.gov/earthquakes/eventpage/ci11408890", coordinates: [-116.7776667,33.6633333,12.39])
+      let quake = Quake(magnitude: 1.29, locationString: "10km SSW of Pine Cove, California", rawTime: 1388643296020, url:"http://earthquake.usgs.gov/earthquakes/eventpage/ci11408890", coordinates: [-116.7776667,33.6633333,12.39])
       let quakemodel = QuakeViewModel(quake: quake)
       
       it("returns the magnitude to one decimal place") {
@@ -23,6 +23,10 @@ class QuakeViewModelSpec: QuickSpec {
       
       it("returns the location String") {
         expect(quakemodel.locationString) == quake.locationString
+      }
+      
+      it("returns the time as a short string") {
+        expect(quakemodel.time) == "10:14 PM"
       }
     }
   }

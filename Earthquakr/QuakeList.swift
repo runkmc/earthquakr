62af6a30ff1ba4c2d74d282bdbc77b4c2ee35a62
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-04 15:59:51 -0800

    QuakeList filters by MMI number

--- Earthquakr/QuakeList.swift
+++ Earthquakr/QuakeList.swift
@@ -7,11 +7,40 @@
 //
 
 import Foundation
+import CoreLocation
 
 struct QuakeList {
   let quakeViewModels: [QuakeViewModel]
+  static let logConstant = log(10.0)
   
   init(quakes: [Quake]) {
     self.quakeViewModels = quakes.map { QuakeViewModel(quake: $0) }
   }
-}
+  
+  func quakesWithMMI(mmiNumber: Double, location: CLLocation) -> [QuakeViewModel] {
+    return self.quakeViewModels.filter { quakeModel in
+      let distance = Int(location.distanceFromLocation(quakeModel.location)) / 1000
+      let magnitude = quakeModel.quake.magnitude
+      if mmiFormula(distance, magnitude: magnitude) > 2.3 {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+  
+  func mmiFormula(distance: Int, magnitude: Double) -> Double {
+    
+    func first() -> Double {
+      return 1.15 + (1.01 * magnitude)
+    }
+    func second() -> Double {
+      return 0.00054 * Double(distance)
+    }
+    func third() -> Double {
+      return 1.72 * log(Double(distance)) / QuakeList.logConstant
+    }
+    
+     return first() - second() - third()
+    }
+  }
--- EarthquakrTests/QuakeListSpec.swift
+++ EarthquakrTests/QuakeListSpec.swift
@@ -9,6 +9,7 @@
 import Foundation
 import Quick
 import Nimble
+import CoreLocation
 @testable import Earthquakr
 
 class QuakeListSpec : QuickSpec {
@@ -17,18 +18,24 @@ class QuakeListSpec : QuickSpec {
     let date1 = Int(xHoursAgo(1)(date: NSDate()).timeIntervalSince1970 * 1000)
     let quake1 = Quake(magnitude: 4.55, locationString: "0km ESE of Concord, California", rawTime:date1, url:"http://earthquake.usgs.gov/earthquakes/eventpage/ak10992884", coordinates:[-116.7776667,33.6633333,12.39])
     let date2 = Int(xHoursAgo(2)(date: NSDate()).timeIntervalSince1970 * 1000)
-    let quake2 = Quake(magnitude: 4.55, locationString: "0km ESE of Concord, California", rawTime:date2, url:"http://earthquake.usgs.gov/earthquakes/eventpage/ak10992884", coordinates:[-121.958000,37.780000,12.39])
+    let quake2 = Quake(magnitude: 2.55, locationString: "0km ESE of Concord, California", rawTime:date2, url:"http://earthquake.usgs.gov/earthquakes/eventpage/ak10992884", coordinates:[-121.958000,37.780000,12.39])
     let date3 = Int(xHoursAgo(3)(date: NSDate()).timeIntervalSince1970 * 1000)
-    let quake3 = Quake(magnitude: 4.55, locationString: "0km ESE of Concord, California", rawTime:date3, url:"http://earthquake.usgs.gov/earthquakes/eventpage/ak10992884", coordinates:[-121,959000,37.780000,12.39])
+    let quake3 = Quake(magnitude: 1.55, locationString: "0km ESE of Concord, California", rawTime:date3, url:"http://earthquake.usgs.gov/earthquakes/eventpage/ak10992884", coordinates:[-121,958000,37.780000,12.39])
     let date4 = Int(xHoursAgo(4)(date: NSDate()).timeIntervalSince1970 * 1000)
-    let quake4 = Quake(magnitude: 4.55, locationString: "0km ESE of Concord, California", rawTime:date4, url:"http://earthquake.usgs.gov/earthquakes/eventpage/ak10992884", coordinates:[-121,957000,37.780000,12.39])
+    let quake4 = Quake(magnitude: 8.55, locationString: "0km ESE of Concord, California", rawTime:date4, url:"http://earthquake.usgs.gov/earthquakes/eventpage/ak10992884", coordinates:[-121,958000,37.780000,12.39])
     let quakes = [quake1, quake2, quake3, quake4]
     
     it("takes an array of Quakes and provides an array of QuakeViewModels") {
       let list = QuakeList(quakes: quakes)
       expect(list.quakeViewModels[0].magnitudeString) == "4.5"
     }
-    
+  
+    it("returns quakes over a given MMI number") {
+      // see http://resilience.abag.ca.gov/shaking/mmi/ for the scale referenced here
+      let list = QuakeList(quakes: quakes)
+      let myLocation = CLLocation(latitude: 37.875920, longitude: -122.071068)
+      expect(list.quakesWithMMI(2.3, location:myLocation).count) == 2
+    }
     
   }
 }

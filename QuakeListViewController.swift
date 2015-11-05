18c2f69a039f1bba3c46170d1bfc4b9321330822
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-05 10:42:57 -0800

    Implement CoreLocation stuff

--- QuakeListViewController.swift
+++ QuakeListViewController.swift
@@ -7,11 +7,46 @@
 //
 
 import UIKit
+import CoreLocation
 
-class QuakeListViewController: UIViewController {
+class QuakeListViewController: UIViewController, CLLocationManagerDelegate {
 
+  let manager = CLLocationManager()
+  @IBOutlet weak var locationLabel: UILabel!
+  @IBOutlet weak var tableView: UITableView!
+  
+  func askForLocation() {
+    manager.delegate = self
+    manager.requestWhenInUseAuthorization()
+    let status = CLLocationManager.authorizationStatus()
+    
+    if (status == .AuthorizedAlways || status == .AuthorizedWhenInUse) {
+      manager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+      manager.requestLocation()
+    } else {
+      locationLabel.text = "Location Services Disabled"
+    }
+  }
+  
+  func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+    if status == .AuthorizedAlways || status == .AuthorizedWhenInUse {
+      locationLabel.text = "Getting Location"
+    } else {
+      locationLabel.text = "Location Services Disabled"
+    }
+  }
+  
+  func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    let location = locations.last
+  }
+  
+  func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+    locationLabel.text = "Location Not Available"
+  }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.askForLocation()
 
         // Do any additional setup after loading the view.
     }
